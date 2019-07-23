57fa0c72a4df50be42da2c4cbf4f086fe9acbbad
Vetaut <olneywill@msn.com>
2019-07-22 21:12:42 -0400

    Update GameManager.cs

--- eArcadeFight/Assets/Scripts/GameManager.cs
+++ eArcadeFight/Assets/Scripts/GameManager.cs
@@ -1,16 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager gameManager;
 
+    public Image[] wUIImage;
+    public Sprite[] wIdleUIImage;
+    public Sprite[] wPressedUIImage;
 
-    // Start is called before the first frame update
-    void Start()
+    public GameObject[] wKnightPrefab;
+    public int charID;
+
+    // Public GameObject PlayerObj;
+    public List<GameObject> wMonsterList = new List<GameObject>();
+    public GameObject[] wRespawnPos;
+    public GameObject wMonsterPrefab;
+    public int respawnID = 0;
+    // Use this for initialization
+    void Awake()
     {
-        
+        Screen.fullScreen = false;
+
+        gameManager = this;
     }
 
     // Update is called once per frame
